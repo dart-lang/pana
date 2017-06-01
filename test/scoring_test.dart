bf2565d3f0b27b073303b48cf092dab4bb33c8db
Kevin Moore <kevmoo@google.com>
2017-06-01 15:25:50 -0700

    Update licensing details, add CONTRIBUTING.md

--- test/scoring_test.dart
+++ test/scoring_test.dart
@@ -1,5 +1,6 @@
-// Copyright (c) 2017, Kevin Moore. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
 
 import 'dart:math';
 import 'package:pana/src/scoring.dart';
