644990e9557df2e1ce381ac3ba90060d0ac660e8
Istvan Soos <istvan.soos@gmail.com>
2017-06-02 07:47:40 +0200

    Move pubspec-parsing utils to pubspec.dart.

--- lib/src/pubspec.dart
+++ lib/src/pubspec.dart
@@ -1,9 +1,26 @@
+import 'dart:collection';
+
+import 'package:yaml/yaml.dart' as yaml;
+
+import 'utils.dart';
+
 class Pubspec {
   final Map<String, dynamic> _data;
 
+  Set<String> _dependentSdks;
+
   Pubspec(this._data);
 
+  factory Pubspec.parseFromDir(String packageDir) {
+    String content = getPubspecContent(packageDir);
+    if (content == null) {
+      throw new Exception("Couldn't find a pubspec.yaml in $packageDir.");
+    }
+    return new Pubspec(yaml.loadYaml(content));
+  }
+
   Map<String, dynamic> get dependencies => _data['dependencies'];
+  Map<String, dynamic> get devDependencies => _data['dev_dependencies'];
 
   bool get hasFlutterKey => _data.containsKey('flutter');
   bool get hasFlutterPluginKey =>
@@ -11,11 +28,7 @@ class Pubspec {
       _data['flutter'] is Map &&
       _data['flutter']['plugin'] != null;
 
-  bool get dependsOnFlutterSdk {
-    if (dependencies == null) return false;
-    return dependencies.values
-        .any((value) => value is Map && value['sdk'] == 'flutter');
-  }
+  bool get dependsOnFlutterSdk => dependentSdks.contains('flutter');
 
   bool get dependsOnAnyVersion {
     if (dependencies == null) return false;
@@ -24,5 +37,28 @@ class Pubspec {
       if (v is Map) return v['version'] == 'any';
       return false;
     });
+  }
+
+  Set<String> get dependentSdks {
+    if (_dependentSdks == null) {
+      _dependentSdks = new SplayTreeSet();
+      dependencies?.values?.forEach((value) {
+        if (value is Map && value['sdk'] != null) {
+          _dependentSdks.add(value['sdk']);
+        }
+      });
+      devDependencies?.values?.forEach((value) {
+        if (value is Map && value['sdk'] != null) {
+          _dependentSdks.add(value['sdk']);
+        }
+      });
+    }
+    return _dependentSdks;
+  }
+
+  bool get hasUnknownSdks {
+    Set<String> sdks = new Set.from(dependentSdks);
+    sdks.remove('flutter');
+    return sdks.isNotEmpty;
   }
 }
