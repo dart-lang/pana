fef01f4b94b86c56b7560a07c81ea09a8e5d8636
Kevin Moore <kevmoo@google.com>
2017-11-17 09:39:48 -0800

    Remove gratuitous group in test

--- test/end2end_test.dart
+++ test/end2end_test.dart
@@ -14,51 +14,49 @@ import 'end2end/shared.dart';
 import 'end2end/skiplist_data.dart' as skiplist_data;
 
 void main() {
-  group('PackageAnalyzer', () {
-    Directory tempDir;
-    PackageAnalyzer analyzer;
+  Directory tempDir;
+  PackageAnalyzer analyzer;
 
-    setUpAll(() async {
-      tempDir = await Directory.systemTemp.createTemp('pana-test');
-      var pubCacheDir = await tempDir.resolveSymbolicLinks();
-      analyzer = new PackageAnalyzer(pubCacheDir: pubCacheDir);
-    });
-
-    tearDownAll(() async {
-      await tempDir.delete(recursive: true);
-    });
-
-    void _verifyPackage(E2EData data) {
-      group('${data.name} ${data.version}', () {
-        Map actualMap;
-
-        setUpAll(() async {
-          var summary = await analyzer.inspectPackage(
-            data.name,
-            version: data.version,
-            keepTransitiveLibs: true,
-          );
-
-          // summary.toJson contains types which are not directly JSON-able
-          // throwing it through `JSON.encode` does the trick
-          actualMap = JSON.decode(JSON.encode(summary));
-        });
-
-        test('matches known good', () {
-          expect(actualMap, data.data);
-        });
-
-        test('Summary can round-trip', () {
-          var summary = new Summary.fromJson(actualMap);
-
-          var roundTrip = JSON.decode(JSON.encode(summary));
-          expect(roundTrip, actualMap);
-        });
-      }, timeout: const Timeout.factor(2));
-    }
-
-    _verifyPackage(pub_server_data.data);
-    _verifyPackage(http_data.data);
-    _verifyPackage(skiplist_data.data);
+  setUpAll(() async {
+    tempDir = await Directory.systemTemp.createTemp('pana-test');
+    var pubCacheDir = await tempDir.resolveSymbolicLinks();
+    analyzer = new PackageAnalyzer(pubCacheDir: pubCacheDir);
   });
+
+  tearDownAll(() async {
+    await tempDir.delete(recursive: true);
+  });
+
+  void _verifyPackage(E2EData data) {
+    group('${data.name} ${data.version}', () {
+      Map actualMap;
+
+      setUpAll(() async {
+        var summary = await analyzer.inspectPackage(
+          data.name,
+          version: data.version,
+          keepTransitiveLibs: true,
+        );
+
+        // summary.toJson contains types which are not directly JSON-able
+        // throwing it through `JSON.encode` does the trick
+        actualMap = JSON.decode(JSON.encode(summary));
+      });
+
+      test('matches known good', () {
+        expect(actualMap, data.data);
+      });
+
+      test('Summary can round-trip', () {
+        var summary = new Summary.fromJson(actualMap);
+
+        var roundTrip = JSON.decode(JSON.encode(summary));
+        expect(roundTrip, actualMap);
+      });
+    }, timeout: const Timeout.factor(2));
+  }
+
+  _verifyPackage(pub_server_data.data);
+  _verifyPackage(http_data.data);
+  _verifyPackage(skiplist_data.data);
 }
