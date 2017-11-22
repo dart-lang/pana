04be50ebf8d6b01acd186db7740223ad591a9b08
Kevin Moore <kevmoo@google.com>
2017-11-22 14:49:10 -0800

    prepare for 0.7.0

--- lib/src/version.g.dart
+++ lib/src/version.g.dart
@@ -10,4 +10,4 @@ part of pana.version;
 // Generator: PackageVersionGenerator
 // **************************************************************************
 
-final _$panaPkgVersionPubSemverVersion = new Version.parse("0.7.0-dev");
+final _$panaPkgVersionPubSemverVersion = new Version.parse("0.7.0");
