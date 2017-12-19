fa506ad9e2500c9539b11ddc55dbb9571fe5f194
István Soós <isoos@users.noreply.github.com>
2017-12-19 20:41:52 +0100

    Flutter-specific dartanalyzer options. (#191)

--- lib/src/analysis_options.dart
+++ lib/src/analysis_options.dart
@@ -18,3 +18,33 @@ linter:
     - unrelated_type_equality_checks
     - valid_regexps
 ''';
+
+// Keep it updated with
+// https://github.com/flutter/flutter/blob/master/packages/flutter/lib/analysis_options_user.yaml
+const String flutterAnalysisOptions = '''
+analyzer:
+  language:
+    enableStrictCallChecks: true
+    enableSuperMixins: true
+    enableAssertInitializer: true
+  strong-mode: true
+  errors:
+    # treat missing required parameters as a warning (not a hint)
+    missing_required_param: warning
+    # treat missing returns as a warning (not a hint)
+    missing_return: warning
+    # allow having TODOs in the code
+    todo: ignore
+
+# Source of linter options:
+# http://dart-lang.github.io/linter/lints/options/options.html
+
+linter:
+  rules:
+    - camel_case_types
+    - hash_and_equals
+    - iterable_contains_unrelated_type
+    - list_remove_unrelated_type
+    - unrelated_type_equality_checks
+    - valid_regexps
+''';
