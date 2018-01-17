003821abbffd16859f845f3dda4234d6d2345afd
Istvan Soos <istvan.soos@gmail.com>
2018-01-17 22:22:02 +0100

    Adapt new structure.

--- lib/src/platform.dart
+++ lib/src/platform.dart
@@ -170,104 +170,100 @@ class PlatformDef {
     PlatformDef.web,
   ];
 
-  static List<PlatformStatus> detectPlatforms(List<ComponentDef> components) =>
-      values.map((p) => p.detectStatus(components)).toList();
+  static Map<String, PlatformUse> detectUses(List<ComponentDef> components) {
+    return new Map<String, PlatformUse>.fromIterable(
+      values,
+      // ignore: uses_dynamic_as_bottom
+      key: (PlatformDef p) => p.name,
+      // ignore: uses_dynamic_as_bottom
+      value: (PlatformDef p) => p.detectUse(components),
+    );
+  }
 
-  PlatformStatus detectStatus(List<ComponentDef> components) {
+  PlatformUse detectUse(List<ComponentDef> components) {
     final isAllowed =
         components.isEmpty || components.every((c) => !forbidden.contains(c));
     final isUsed = components.any((c) => required.contains(c));
-    return new PlatformStatus(name, isAllowed, isUsed);
+    return _getPlatformStatus(isAllowed, isUsed);
   }
 }
 
-@JsonSerializable()
-class PlatformStatus extends Object with _$PlatformStatusSerializerMixin {
-  final String name;
-  final bool isAllowed;
-  final bool isUsed;
+enum PlatformUse {
+  /// is allowed, but not used
+  allowed,
 
-  PlatformStatus(this.name, this.isAllowed, this.isUsed);
-  factory PlatformStatus.fromJson(Map<String, dynamic> json) =>
-      _$PlatformStatusFromJson(json);
+  /// is allowed AND used
+  used,
 
-  bool get hasConflict => !isAllowed && isUsed;
+  /// is forbidden AND used
+  conflict,
+
+  /// is forbidden, but not used
+  forbidden,
 }
+
+PlatformUse _getPlatformStatus(bool isAllowed, bool isUsed) {
+  if (isAllowed) {
+    return isUsed ? PlatformUse.used : PlatformUse.allowed;
+  } else {
+    return isUsed ? PlatformUse.conflict : PlatformUse.forbidden;
+  }
+}
+
+bool _isAllowed(PlatformUse value) =>
+    value == PlatformUse.allowed || value == PlatformUse.used;
 
 @JsonSerializable()
 class DartPlatform extends Object with _$DartPlatformSerializerMixin {
-  final bool worksEverywhere;
+  @JsonKey(includeIfNull: false)
+  final List<String> components;
 
   @JsonKey(includeIfNull: false)
-  final List<String> restrictedTo;
+  final Map<String, PlatformUse> uses;
 
   @JsonKey(includeIfNull: false)
   final String reason;
 
-  DartPlatform(this.worksEverywhere, this.restrictedTo, this.reason);
+  DartPlatform(this.components, this.uses, {this.reason});
 
   factory DartPlatform.conflict(String reason) =>
-      new DartPlatform(false, null, reason);
+      new DartPlatform(null, null, reason: reason);
 
-  factory DartPlatform.universal({String reason}) =>
-      new DartPlatform(true, null, reason);
+  factory DartPlatform.fromComponents(List<String> components,
+      {String reason}) {
+    final defs = components
+        .map((c) => ComponentDef.values.singleWhere((def) => def.name == c))
+        .toList();
+    return new DartPlatform(components, PlatformDef.detectUses(defs),
+        reason: reason);
+  }
 
-  factory DartPlatform.withRestrictions(List<String> restrictedTo,
-          {String reason}) =>
-      new DartPlatform(false, restrictedTo, reason);
+  factory DartPlatform.everywhere(String reason) =>
+      new DartPlatform.fromComponents([], reason: reason);
 
   factory DartPlatform.fromJson(Map<String, dynamic> json) =>
       _$DartPlatformFromJson(json);
 
+  bool get worksEverywhere =>
+      uses != null && uses.values.every((s) => _isAllowed(s));
   bool get worksAnywhere =>
-      worksEverywhere || (restrictedTo != null && restrictedTo.isNotEmpty);
+      uses != null && uses.values.any((s) => _isAllowed(s));
   bool get hasConflict => !worksAnywhere;
 
   bool get worksOnFlutter => _worksOn(PlatformNames.flutter);
   bool get worksOnServer => _worksOn(PlatformNames.server);
   bool get worksOnWeb => _worksOn(PlatformNames.web);
 
-  bool _worksOn(String name) =>
-      worksEverywhere || (restrictedTo != null && restrictedTo.contains(name));
-}
+  bool get usesFlutter => _uses(PlatformNames.flutter);
 
-class _LibInspector {
-  final Set<String> _deps;
-  _LibInspector._(this._deps);
+  /// Visible for testing only, DO NOT USE in clients.
+  String get longPlatformDebug => uses.keys
+      .map((k) => '$k: ${uses[k].toString().split('.').last}')
+      .join(', ');
 
-  factory _LibInspector(Set<String> dependencies) =>
-      new _LibInspector._(_normalizeDependencies(dependencies));
+  bool _worksOn(String name) => uses != null && _isAllowed(uses[name]);
 
-  bool get hasConflict =>
-      (!worksAnywhere) ||
-      (_deps.contains(PlatformNames.flutter) && !worksOnFlutter) ||
-      ((_deps.contains('dart-ext:') || _deps.contains('dart:nativewrappers')) &&
-          !worksOnServer);
-
-  bool get worksEverywhere => worksOnWeb && worksOnServer && worksOnFlutter;
-
-  bool get worksAnywhere => worksOnWeb || worksOnServer || worksOnFlutter;
-
-  bool get worksOnWeb =>
-      _hasNoUseOf([
-        PlatformNames.flutter,
-        'dart:ui',
-        'dart-ext:',
-        'dart:nativewrappers',
-      ]) &&
-      (_webPackages.any(_deps.contains) || _hasNoUseOf(['dart:io']));
-
-  bool get worksOnServer =>
-      _hasNoUseOf(_webAnd(['dart:ui', PlatformNames.flutter]));
-
-  bool get worksOnFlutter => _hasNoUseOf(_webAnd([
-        'dart:mirrors',
-        'dart-ext:',
-        'dart:nativewrappers',
-      ]));
-
-  bool _hasNoUseOf(Iterable<String> platforms) =>
-      !platforms.any((p) => _deps.contains(p));
+  bool _uses(String name) => uses != null && uses[name] == PlatformUse.used;
 }
 
 DartPlatform classifyPkgPlatform(
@@ -275,16 +271,41 @@ DartPlatform classifyPkgPlatform(
   if (transitiveLibs == null) {
     return new DartPlatform.conflict('Failed to scan transitive libraries.');
   }
+
   final libraries = new Map<String, DartPlatform>.fromIterable(
       transitiveLibs.keys ?? <String>[],
       value: (key) => classifyLibPlatform(transitiveLibs[key]));
-  final primaryLibrary =
-      _selectPrimaryLibrary(pubspec, transitiveLibs.keys.toSet());
 
-  if (pubspec.isFlutter) {
+  final conflicts =
+      libraries.keys.where((key) => libraries[key].hasConflict).toList();
+  if (conflicts.isNotEmpty) {
+    conflicts.sort();
+    var sample = conflicts.take(3).map((s) => '`$s`').join(', ');
+    if (conflicts.length > 3) {
+      sample = '$sample (and ${conflicts.length - 3} more).';
+    }
+    return new DartPlatform.conflict('Conflicting libraries: $sample.');
+  }
+
+  final allComponentsSet = new Set<String>();
+  libraries.values
+      .map((p) => p.components)
+      .where((c) => c != null)
+      .forEach(allComponentsSet.addAll);
+  final allComponentNames = allComponentsSet.toList()..sort();
+
+  final usesFlutter = libraries.values.any((p) => p.usesFlutter);
+  if (pubspec.isFlutter || usesFlutter) {
     final flutterConflicts =
         libraries.keys.where((key) => !libraries[key].worksOnFlutter).toList();
-    if (flutterConflicts.isNotEmpty) {
+    if (flutterConflicts.isEmpty) {
+      final withFlutter = new Set<String>.from(allComponentsSet)
+        ..add(ComponentNames.flutter);
+      return new DartPlatform.fromComponents(
+        withFlutter.toList()..sort(),
+        reason: 'References Flutter, and has no conflicting libraries.',
+      );
+    } else {
       flutterConflicts.sort();
       var sample = flutterConflicts.take(3).map((s) => '`$s`').join(', ');
       if (flutterConflicts.length > 3) {
@@ -292,70 +313,33 @@ DartPlatform classifyPkgPlatform(
       }
       return new DartPlatform.conflict(
           'References Flutter, but has conflicting libraries: $sample.');
-    } else {
-      return new DartPlatform.withRestrictions([PlatformNames.flutter],
-          reason: 'References Flutter, and has no conflicting libraries.');
     }
   }
 
-  if (libraries.isEmpty) {
-    // TODO: if there is a `bin/` asset, maybe this is server-only?
-    return new DartPlatform.conflict('No libraries!');
-  }
-
-  for (var lib in libraries.keys) {
-    final libp = libraries[lib];
-    if (libp.hasConflict) {
-      return new DartPlatform.conflict('Conflict in library `$lib`.');
-    }
-  }
-
-  if (libraries.values.every((lp) => lp.worksEverywhere)) {
-    return new DartPlatform.universal(
-        reason: 'No platform restriction found in libraries.');
-  }
-
-  if (primaryLibrary != null && libraries[primaryLibrary].worksEverywhere) {
-    return new DartPlatform.universal(
-        reason:
-            'No platform restriction found in primary library `$primaryLibrary`.');
-  }
-
-  final items = libraries.values
-      .where((p) => !p.worksEverywhere)
-      .expand((p) => p.restrictedTo)
-      .toSet();
-  if (items.length == 1) {
-    return new DartPlatform.withRestrictions([items.single],
-        reason: 'All libraries have the same platform restriction.');
-  }
-
+  final primaryLibrary =
+      _selectPrimaryLibrary(pubspec, transitiveLibs.keys.toSet());
   if (primaryLibrary != null) {
-    var primaryPlatform = libraries[primaryLibrary];
-    if (primaryPlatform.restrictedTo?.length == 1) {
-      final platform = primaryPlatform.restrictedTo.single;
-      return new DartPlatform.withRestrictions(primaryPlatform.restrictedTo,
-          reason:
-              'Primary library `$primaryLibrary` is restricted to `$platform`.');
+    final primaryPlatform = libraries[primaryLibrary];
+    if (primaryPlatform.worksEverywhere) {
+      return new DartPlatform.everywhere(
+          'No platform restriction found in primary library `$primaryLibrary`.');
+    } else {
+      return new DartPlatform.fromComponents(primaryPlatform.components,
+          reason: 'Primary library: `$primaryLibrary`.');
     }
   }
 
-  // If the primary library search fails, go back to the roll-up of all
-  // platforms.
-
-  if (items.isEmpty) {
-    return new DartPlatform.conflict(
-        'Unable to identify platform from libraries (no restriction found).');
+  if (transitiveLibs.isEmpty) {
+    return new DartPlatform.everywhere('No libraries.');
   }
 
-  if (items.length == 1) {
-    final platform = items.single;
-    return new DartPlatform.withRestrictions([items.single],
-        reason: 'Libraries are restricted to `$platform`.');
+  if (allComponentsSet.isEmpty) {
+    return new DartPlatform.everywhere(
+        'No platform restriction found in libraries.');
+  } else {
+    return new DartPlatform.fromComponents(allComponentNames,
+        reason: 'Multiple platform identified in libraries.');
   }
-
-  return new DartPlatform.withRestrictions(items.toList()..sort(),
-      reason: 'Multiple platform identified in libraries.');
 }
 
 String _selectPrimaryLibrary(Pubspec pubspec, Set<String> libraryUris) {
@@ -368,37 +352,11 @@ String _selectPrimaryLibrary(Pubspec pubspec, Set<String> libraryUris) {
 }
 
 DartPlatform classifyLibPlatform(Iterable<String> dependencies) {
-  final inspector = new _LibInspector(dependencies.toSet());
-  if (inspector.hasConflict) {
-    return new DartPlatform.conflict('Transitive dependencies in conflict.');
-  }
-  if (inspector.worksEverywhere) {
-    return new DartPlatform.universal();
-  }
-  final restrictedTo = <String>[];
-  if (inspector.worksOnFlutter) restrictedTo.add(PlatformNames.flutter);
-  if (inspector.worksOnServer) restrictedTo.add(PlatformNames.server);
-  if (inspector.worksOnWeb) restrictedTo.add(PlatformNames.web);
-  restrictedTo.sort();
-  if (restrictedTo.isEmpty) {
-    return new DartPlatform.conflict('Transitive dependencies in conflict.');
-  }
-  return new DartPlatform.withRestrictions(restrictedTo);
+  final components = ComponentDef.detectComponents(dependencies);
+  final platforms = PlatformDef.detectUses(components);
+  final componentNames = components.map((c) => c.name).toList();
+  return new DartPlatform(componentNames, platforms);
 }
-
-Iterable<String> _webAnd(Iterable<String> other) =>
-    [_webPackages, other].expand((s) => s);
-
-const List<String> _webPackages = const [
-  'dart:html',
-  'dart:indexed_db',
-  'dart:js',
-  'dart:js_util',
-  'dart:svg',
-  'dart:web_audio',
-  'dart:web_gl',
-  'dart:web_sql',
-];
 
 Set<String> _normalizeDependencies(Iterable<String> dependencies) {
   var deps = new Set<String>();
