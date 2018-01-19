354d16158206d9a9a6da94980d95215aa7c92b15
István Soós <isoos@users.noreply.github.com>
2018-01-19 12:30:20 +0100

    Prepare 0.10.0 release (#234)

--- lib/src/version.g.dart
+++ lib/src/version.g.dart
@@ -10,4 +10,4 @@ part of pana.version;
 // Generator: PackageVersionGenerator
 // **************************************************************************
 
-final _$panaPkgVersionPubSemverVersion = new Version.parse("0.10.0-dev");
+final _$panaPkgVersionPubSemverVersion = new Version.parse("0.10.0");
