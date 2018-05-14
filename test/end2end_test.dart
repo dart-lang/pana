3ed28b781d9b1eeeebc74f4d047d3a3a528a54f2
István Soós <isoos@users.noreply.github.com>
2018-05-14 10:09:23 +0200

    Refactor end2end tests. (#313)

--- test/end2end_test.dart
+++ test/end2end_test.dart
@@ -5,14 +5,15 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:pana/pana.dart';
+import 'package:pub_semver/pub_semver.dart';
 import 'package:test/test.dart';
 
-import 'end2end/http_data.dart' as http_data;
-import 'end2end/pub_server_data.dart' as pub_server_data;
-import 'end2end/shared.dart';
-import 'end2end/skiplist_data.dart' as skiplist_data;
-import 'end2end/stream_broken_data.dart' as stream_broken_data;
+import 'package:pana/pana.dart';
+import 'package:pana/src/version.dart';
+
+const String goldenDir = 'test/end2end';
+
+final _regenerateGoldens = false;
 
 void main() {
   Directory tempDir;
@@ -31,14 +32,14 @@ void main() {
     await tempDir.delete(recursive: true);
   });
 
-  void _verifyPackage(E2EData data) {
-    group('${data.name} ${data.version}', () {
+  void _verifyPackage(String fileName, String package, String version) {
+    group('end2end: $package $version', () {
       Map actualMap;
 
       setUpAll(() async {
         var summary = await analyzer.inspectPackage(
-          data.name,
-          version: data.version,
+          package,
+          version: version,
           options: new InspectOptions(
             verbosity: Verbosity.verbose,
             dartdocOutputDir: '$rootPath/dartdoc',
@@ -51,7 +52,42 @@ void main() {
       });
 
       test('matches known good', () {
-        expect(actualMap, data.data);
+        final file = new File('$goldenDir/$fileName');
+        if (_regenerateGoldens) {
+          final content = new JsonEncoder.withIndent('  ').convert(actualMap);
+          file.writeAsStringSync(content);
+          fail('Set `_regenerateGoldens` to `false` to run tests.');
+        }
+
+        final Map content = json.decode(file.readAsStringSync());
+        content['runtimeInfo']['panaVersion'] =
+            matches(panaPkgVersion.toString());
+
+        // TODO: allow future versions and remove this override
+        content['runtimeInfo']['sdkVersion'] = isSemVer;
+
+        if (content.containsKey('pkgResolution') &&
+            content['pkgResolution'].containsKey('dependencies')) {
+          content['pkgResolution']['dependencies']?.forEach((Map map) {
+            // TODO: allow future versions and remove this override
+            if (map.containsKey('resolved')) {
+              map['resolved'] = isNotNull;
+            }
+            // TODO: allow future versions and remove this override
+            if (map.containsKey('available')) {
+              map['available'] = isNotNull;
+            }
+          });
+        }
+
+        if (content.containsKey('suggestions')) {
+          content['suggestions']?.forEach((Map map) {
+            // TODO: normalize paths in error reports and remove this override
+            map['description'] = isNotEmpty;
+          });
+        }
+
+        expect(actualMap, content);
       });
 
       test('Summary can round-trip', () {
@@ -63,8 +99,17 @@ void main() {
     }, timeout: const Timeout.factor(2));
   }
 
-  _verifyPackage(pub_server_data.data);
-  _verifyPackage(http_data.data);
-  _verifyPackage(skiplist_data.data);
-  _verifyPackage(stream_broken_data.data);
+  _verifyPackage('http-0.11.3-13.json', 'http', '0.11.3+13');
+  _verifyPackage('pub_server-0.1.1-3.json', 'pub_server', '0.1.1+3');
+  _verifyPackage('skiplist-0.1.0.json', 'skiplist', '0.1.0');
+  _verifyPackage('stream-0.7.2-2.json', 'stream', '0.7.2+2');
 }
+
+Matcher isSemVer = predicate<String>((String versionString) {
+  try {
+    new Version.parse(versionString);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}, 'can be parsed as a version');
