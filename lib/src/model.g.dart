62697a3cfa3e6874e4780a34455e41eaa7273cf1
Kevin Moore <kevmoo@google.com>
2019-07-29 17:59:34 -0700

    regenerate JSON code

--- lib/src/model.g.dart
+++ lib/src/model.g.dart
@@ -8,46 +8,45 @@ part of 'model.dart';
 
 Summary _$SummaryFromJson(Map<String, dynamic> json) {
   return Summary(
-      runtimeInfo:
-          PanaRuntimeInfo.fromJson(json['runtimeInfo'] as Map<String, dynamic>),
-      packageName: json['packageName'] as String,
-      packageVersion:
-          const VersionConverter().fromJson(json['packageVersion'] as String),
-      pubspec: json['pubspec'] == null
-          ? null
-          : Pubspec.fromJson(json['pubspec'] as Map<String, dynamic>),
-      pkgResolution: json['pkgResolution'] == null
-          ? null
-          : PkgResolution.fromJson(
-              json['pkgResolution'] as Map<String, dynamic>),
-      dartFiles: (json['dartFiles'] as Map<String, dynamic>)?.map(
-        (k, e) => MapEntry(
-            k,
-            e == null
-                ? null
-                : DartFileSummary.fromJson(e as Map<String, dynamic>)),
-      ),
-      platform: json['platform'] == null
-          ? null
-          : DartPlatform.fromJson(json['platform'] as Map<String, dynamic>),
-      licenses: (json['licenses'] as List)
-          ?.map((e) => e == null
+    runtimeInfo:
+        PanaRuntimeInfo.fromJson(json['runtimeInfo'] as Map<String, dynamic>),
+    packageName: json['packageName'] as String,
+    packageVersion:
+        const VersionConverter().fromJson(json['packageVersion'] as String),
+    pubspec: json['pubspec'] == null
+        ? null
+        : Pubspec.fromJson(json['pubspec'] as Map<String, dynamic>),
+    pkgResolution: json['pkgResolution'] == null
+        ? null
+        : PkgResolution.fromJson(json['pkgResolution'] as Map<String, dynamic>),
+    dartFiles: (json['dartFiles'] as Map<String, dynamic>)?.map(
+      (k, e) => MapEntry(
+          k,
+          e == null
               ? null
-              : LicenseFile.fromJson(e as Map<String, dynamic>))
-          ?.toList(),
-      health: json['health'] == null
-          ? null
-          : Health.fromJson(json['health'] as Map<String, dynamic>),
-      maintenance: json['maintenance'] == null
-          ? null
-          : Maintenance.fromJson(json['maintenance'] as Map<String, dynamic>),
-      suggestions: (json['suggestions'] as List)
-          ?.map((e) =>
-              e == null ? null : Suggestion.fromJson(e as Map<String, dynamic>))
-          ?.toList(),
-      stats: json['stats'] == null
-          ? null
-          : Stats.fromJson(json['stats'] as Map<String, dynamic>));
+              : DartFileSummary.fromJson(e as Map<String, dynamic>)),
+    ),
+    platform: json['platform'] == null
+        ? null
+        : DartPlatform.fromJson(json['platform'] as Map<String, dynamic>),
+    licenses: (json['licenses'] as List)
+        ?.map((e) =>
+            e == null ? null : LicenseFile.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    health: json['health'] == null
+        ? null
+        : Health.fromJson(json['health'] as Map<String, dynamic>),
+    maintenance: json['maintenance'] == null
+        ? null
+        : Maintenance.fromJson(json['maintenance'] as Map<String, dynamic>),
+    suggestions: (json['suggestions'] as List)
+        ?.map((e) =>
+            e == null ? null : Suggestion.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    stats: json['stats'] == null
+        ? null
+        : Stats.fromJson(json['stats'] as Map<String, dynamic>),
+  );
 }
 
 Map<String, dynamic> _$SummaryToJson(Summary instance) {
@@ -78,9 +77,10 @@ Map<String, dynamic> _$SummaryToJson(Summary instance) {
 
 PanaRuntimeInfo _$PanaRuntimeInfoFromJson(Map<String, dynamic> json) {
   return PanaRuntimeInfo(
-      panaVersion: json['panaVersion'] as String,
-      sdkVersion: json['sdkVersion'] as String,
-      flutterVersions: json['flutterVersions'] as Map<String, dynamic>);
+    panaVersion: json['panaVersion'] as String,
+    sdkVersion: json['sdkVersion'] as String,
+    flutterVersions: json['flutterVersions'] as Map<String, dynamic>,
+  );
 }
 
 Map<String, dynamic> _$PanaRuntimeInfoToJson(PanaRuntimeInfo instance) {
@@ -101,21 +101,20 @@ Map<String, dynamic> _$PanaRuntimeInfoToJson(PanaRuntimeInfo instance) {
 
 DartFileSummary _$DartFileSummaryFromJson(Map<String, dynamic> json) {
   return DartFileSummary(
-      uri: json['uri'] as String,
-      size: json['size'] as int,
-      isFormatted: json['isFormatted'] as bool,
-      codeProblems: (json['codeProblems'] as List)
-          ?.map((e) => e == null
-              ? null
-              : CodeProblem.fromJson(e as Map<String, dynamic>))
-          ?.toList(),
-      directLibs:
-          (json['directLibs'] as List)?.map((e) => e as String)?.toList(),
-      transitiveLibs:
-          (json['transitiveLibs'] as List)?.map((e) => e as String)?.toList(),
-      platform: json['platform'] == null
-          ? null
-          : DartPlatform.fromJson(json['platform'] as Map<String, dynamic>));
+    uri: json['uri'] as String,
+    size: json['size'] as int,
+    isFormatted: json['isFormatted'] as bool,
+    codeProblems: (json['codeProblems'] as List)
+        ?.map((e) =>
+            e == null ? null : CodeProblem.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    directLibs: (json['directLibs'] as List)?.map((e) => e as String)?.toList(),
+    transitiveLibs:
+        (json['transitiveLibs'] as List)?.map((e) => e as String)?.toList(),
+    platform: json['platform'] == null
+        ? null
+        : DartPlatform.fromJson(json['platform'] as Map<String, dynamic>),
+  );
 }
 
 Map<String, dynamic> _$DartFileSummaryToJson(DartFileSummary instance) {
@@ -139,9 +138,14 @@ Map<String, dynamic> _$DartFileSummaryToJson(DartFileSummary instance) {
 }
 
 Suggestion _$SuggestionFromJson(Map<String, dynamic> json) {
-  return Suggestion(json['code'] as String, json['level'] as String,
-      json['title'] as String, json['description'] as String,
-      file: json['file'] as String, score: (json['score'] as num)?.toDouble());
+  return Suggestion(
+    json['code'] as String,
+    json['level'] as String,
+    json['title'] as String,
+    json['description'] as String,
+    file: json['file'] as String,
+    score: (json['score'] as num)?.toDouble(),
+  );
 }
 
 Map<String, dynamic> _$SuggestionToJson(Suggestion instance) {
@@ -165,11 +169,12 @@ Map<String, dynamic> _$SuggestionToJson(Suggestion instance) {
 
 DartPlatform _$DartPlatformFromJson(Map<String, dynamic> json) {
   return DartPlatform(
-      (json['components'] as List)?.map((e) => e as String)?.toList(),
-      (json['uses'] as Map<String, dynamic>)?.map(
-        (k, e) => MapEntry(k, _$enumDecodeNullable(_$PlatformUseEnumMap, e)),
-      ),
-      reason: json['reason'] as String);
+    (json['components'] as List)?.map((e) => e as String)?.toList(),
+    (json['uses'] as Map<String, dynamic>)?.map(
+      (k, e) => MapEntry(k, _$enumDecodeNullable(_$PlatformUseEnumMap, e)),
+    ),
+    reason: json['reason'] as String,
+  );
 }
 
 Map<String, dynamic> _$DartPlatformToJson(DartPlatform instance) {
@@ -216,25 +221,31 @@ const _$PlatformUseEnumMap = <PlatformUse, dynamic>{
 };
 
 PkgResolution _$PkgResolutionFromJson(Map<String, dynamic> json) {
-  return PkgResolution((json['dependencies'] as List)
-      ?.map((e) =>
-          e == null ? null : PkgDependency.fromJson(e as Map<String, dynamic>))
-      ?.toList());
+  return PkgResolution(
+    (json['dependencies'] as List)
+        ?.map((e) => e == null
+            ? null
+            : PkgDependency.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
 }
 
 Map<String, dynamic> _$PkgResolutionToJson(PkgResolution instance) =>
-    <String, dynamic>{'dependencies': instance.dependencies};
+    <String, dynamic>{
+      'dependencies': instance.dependencies,
+    };
 
 PkgDependency _$PkgDependencyFromJson(Map<String, dynamic> json) {
   return PkgDependency(
-      package: json['package'] as String,
-      dependencyType: json['dependencyType'] as String,
-      constraintType: json['constraintType'] as String,
-      constraint: const VersionConstraintConverter()
-          .fromJson(json['constraint'] as String),
-      resolved: const VersionConverter().fromJson(json['resolved'] as String),
-      available: const VersionConverter().fromJson(json['available'] as String),
-      errors: (json['errors'] as List)?.map((e) => e as String)?.toList());
+    package: json['package'] as String,
+    dependencyType: json['dependencyType'] as String,
+    constraintType: json['constraintType'] as String,
+    constraint: const VersionConstraintConverter()
+        .fromJson(json['constraint'] as String),
+    resolved: const VersionConverter().fromJson(json['resolved'] as String),
+    available: const VersionConverter().fromJson(json['available'] as String),
+    errors: (json['errors'] as List)?.map((e) => e as String)?.toList(),
+  );
 }
 
 Map<String, dynamic> _$PkgDependencyToJson(PkgDependency instance) {
@@ -261,17 +272,18 @@ Map<String, dynamic> _$PkgDependencyToJson(PkgDependency instance) {
 
 Health _$HealthFromJson(Map<String, dynamic> json) {
   return Health(
-      analyzeProcessFailed: json['analyzeProcessFailed'] as bool,
-      formatProcessFailed: json['formatProcessFailed'] as bool,
-      resolveProcessFailed: json['resolveProcessFailed'] as bool,
-      analyzerErrorCount: json['analyzerErrorCount'] as int,
-      analyzerWarningCount: json['analyzerWarningCount'] as int,
-      analyzerHintCount: json['analyzerHintCount'] as int,
-      platformConflictCount: json['platformConflictCount'] as int,
-      suggestions: (json['suggestions'] as List)
-          ?.map((e) =>
-              e == null ? null : Suggestion.fromJson(e as Map<String, dynamic>))
-          ?.toList());
+    analyzeProcessFailed: json['analyzeProcessFailed'] as bool,
+    formatProcessFailed: json['formatProcessFailed'] as bool,
+    resolveProcessFailed: json['resolveProcessFailed'] as bool,
+    analyzerErrorCount: json['analyzerErrorCount'] as int,
+    analyzerWarningCount: json['analyzerWarningCount'] as int,
+    analyzerHintCount: json['analyzerHintCount'] as int,
+    platformConflictCount: json['platformConflictCount'] as int,
+    suggestions: (json['suggestions'] as List)
+        ?.map((e) =>
+            e == null ? null : Suggestion.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
 }
 
 Map<String, dynamic> _$HealthToJson(Health instance) {
@@ -297,19 +309,20 @@ Map<String, dynamic> _$HealthToJson(Health instance) {
 
 Maintenance _$MaintenanceFromJson(Map<String, dynamic> json) {
   return Maintenance(
-      missingChangelog: json['missingChangelog'] as bool,
-      missingExample: json['missingExample'] as bool,
-      missingReadme: json['missingReadme'] as bool,
-      missingAnalysisOptions: json['missingAnalysisOptions'] as bool,
-      oldAnalysisOptions: json['oldAnalysisOptions'] as bool,
-      strongModeEnabled: json['strongModeEnabled'] as bool,
-      isExperimentalVersion: json['isExperimentalVersion'] as bool,
-      isPreReleaseVersion: json['isPreReleaseVersion'] as bool,
-      dartdocSuccessful: json['dartdocSuccessful'] as bool,
-      suggestions: (json['suggestions'] as List)
-          ?.map((e) =>
-              e == null ? null : Suggestion.fromJson(e as Map<String, dynamic>))
-          ?.toList());
+    missingChangelog: json['missingChangelog'] as bool,
+    missingExample: json['missingExample'] as bool,
+    missingReadme: json['missingReadme'] as bool,
+    missingAnalysisOptions: json['missingAnalysisOptions'] as bool,
+    oldAnalysisOptions: json['oldAnalysisOptions'] as bool,
+    strongModeEnabled: json['strongModeEnabled'] as bool,
+    isExperimentalVersion: json['isExperimentalVersion'] as bool,
+    isPreReleaseVersion: json['isPreReleaseVersion'] as bool,
+    dartdocSuccessful: json['dartdocSuccessful'] as bool,
+    suggestions: (json['suggestions'] as List)
+        ?.map((e) =>
+            e == null ? null : Suggestion.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
 }
 
 Map<String, dynamic> _$MaintenanceToJson(Maintenance instance) {
@@ -336,8 +349,12 @@ Map<String, dynamic> _$MaintenanceToJson(Maintenance instance) {
 }
 
 LicenseFile _$LicenseFileFromJson(Map<String, dynamic> json) {
-  return LicenseFile(json['path'] as String, json['name'] as String,
-      version: json['version'] as String, url: json['url'] as String);
+  return LicenseFile(
+    json['path'] as String,
+    json['name'] as String,
+    version: json['version'] as String,
+    url: json['url'] as String,
+  );
 }
 
 Map<String, dynamic> _$LicenseFileToJson(LicenseFile instance) {
@@ -359,13 +376,14 @@ Map<String, dynamic> _$LicenseFileToJson(LicenseFile instance) {
 
 CodeProblem _$CodeProblemFromJson(Map<String, dynamic> json) {
   return CodeProblem(
-      severity: json['severity'] as String,
-      errorType: json['errorType'] as String,
-      errorCode: json['errorCode'] as String,
-      description: json['description'] as String,
-      file: json['file'] as String,
-      line: json['line'] as int,
-      col: json['col'] as int);
+    severity: json['severity'] as String,
+    errorType: json['errorType'] as String,
+    errorCode: json['errorCode'] as String,
+    description: json['description'] as String,
+    file: json['file'] as String,
+    line: json['line'] as int,
+    col: json['col'] as int,
+  );
 }
 
 Map<String, dynamic> _$CodeProblemToJson(CodeProblem instance) =>
@@ -376,20 +394,21 @@ Map<String, dynamic> _$CodeProblemToJson(CodeProblem instance) =>
       'file': instance.file,
       'line': instance.line,
       'col': instance.col,
-      'description': instance.description
+      'description': instance.description,
     };
 
 Stats _$StatsFromJson(Map<String, dynamic> json) {
   return Stats(
-      analyzeProcessElapsed: json['analyzeProcessElapsed'] as int,
-      formatProcessElapsed: json['formatProcessElapsed'] as int,
-      resolveProcessElapsed: json['resolveProcessElapsed'] as int,
-      totalElapsed: json['totalElapsed'] as int);
+    analyzeProcessElapsed: json['analyzeProcessElapsed'] as int,
+    formatProcessElapsed: json['formatProcessElapsed'] as int,
+    resolveProcessElapsed: json['resolveProcessElapsed'] as int,
+    totalElapsed: json['totalElapsed'] as int,
+  );
 }
 
 Map<String, dynamic> _$StatsToJson(Stats instance) => <String, dynamic>{
       'analyzeProcessElapsed': instance.analyzeProcessElapsed,
       'formatProcessElapsed': instance.formatProcessElapsed,
       'resolveProcessElapsed': instance.resolveProcessElapsed,
-      'totalElapsed': instance.totalElapsed
+      'totalElapsed': instance.totalElapsed,
     };
