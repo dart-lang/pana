1e63324df37cca5f7393828cc21c68592bf5c7a3
Sigurd Meldgaard <sigurdm@google.com>
2020-06-15 11:28:51 +0200

    Remove skip

--- test/ensure_build_test.dart
+++ test/ensure_build_test.dart
@@ -1,8 +1,4 @@
 @Tags(['presubmit-only'])
-@OnPlatform({
-  // TODO(sigurdm): Investigate. Perhaps to do with line-endings?
-  'windows': Skip('The build on windows does not come out clean')
-})
 import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
