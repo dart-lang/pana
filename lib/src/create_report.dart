1364ad4df05a0b6a3f4b7f4ff5b23a93cd625e7b
Kevin Moore <kevmoo@google.com>
2020-08-03 10:56:51 -0700

    dartfmt

--- lib/src/create_report.dart
+++ lib/src/create_report.dart
@@ -962,8 +962,10 @@ class _Issue implements _Paragraph {
       description,
       '</summary>',
       '', // This empty line will make the span render its markdown.
-      if (span != null) span.markdown(basePath: basePath),
-      if (suggestion != null) suggestion,
+      if (span != null)
+        span.markdown(basePath: basePath),
+      if (suggestion != null)
+        suggestion,
       '</details>',
     ].join('\n');
   }
