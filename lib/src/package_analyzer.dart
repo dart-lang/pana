8f8e27ded4a8be19ad3f79e5a0ca21b16d41d48f
Sigurd Meldgaard <sigurdm@google.com>
2021-02-26 10:52:56 +0100

    Propagate error-message

--- lib/src/package_analyzer.dart
+++ lib/src/package_analyzer.dart
@@ -13,6 +13,7 @@ import 'download_utils.dart';
 import 'license.dart';
 import 'logging.dart';
 import 'maintenance.dart';
+import 'messages.dart';
 import 'model.dart';
 import 'pubspec.dart';
 import 'sdk_env.dart';
@@ -144,8 +145,10 @@ class PackageAnalyzer {
       if (dartFiles.isNotEmpty) {
         try {
           analyzerItems = await context.staticAnalysis();
-          // ignore: empty_catches
-        } on ToolException {}
+        } on ToolException catch (e) {
+          context.errors
+              .add(runningDartanalyzerFailed(context.usesFlutter, e.message));
+        }
       }
       if (analyzerItems != null && !analyzerItems.any((item) => item.isError)) {
         final tagger = Tagger(pkgDir);
