e3837e0486342496947c4a0b176f01f50ea01388
István Soós <isoos@users.noreply.github.com>
2021-06-16 14:07:17 +0200

    batch run command to verify if pana is working without exceptions (#912)

--- bin/batch.dart
+++ bin/batch.dart
@@ -7,15 +7,16 @@ import 'package:args/command_runner.dart';
 
 import 'package:pana/src/batch/batch_compare_command.dart';
 import 'package:pana/src/batch/batch_query_command.dart';
+import 'package:pana/src/batch/batch_run_command.dart';
 
 /// Batch analysis and comparison tool.
 ///
 /// 1. Select packages with the query tool:
 ///
-///       dart bin/batch query dart:sdk
-///       dart bin/batch query --sort popularity --limit 10
-///       dart bin/batch query --sort updated --max-age-days 14
-///       dart bin/batch query --output output.txt
+///       dart bin/batch.dart query dart:sdk
+///       dart bin/batch.dart query --sort popularity --limit 10
+///       dart bin/batch.dart query --sort updated --max-age-days 14
+///       dart bin/batch.dart query --output output.txt
 ///
 /// 2. Merge selected packages into a .txt file, one package per line.
 ///
@@ -33,13 +34,17 @@ import 'package:pana/src/batch/batch_query_command.dart';
 ///
 /// 4. Run batch compare:
 ///
-///       dart bin/batch compare --packages packages.txt --experiment experiment.yaml
-///       dart bin/batch compare --package comma,separated,package,list --control old.yaml --experiment new.yaml --output output.json
+///       dart bin/batch.dart compare --packages packages.txt --experiment experiment.yaml
+///       dart bin/batch.dart compare --package comma,separated,package,list --control old.yaml --experiment new.yaml --output output.json
 ///
+///    Or run a single config:
+///
+///       dart bin/batch.dart run --packages packages.txt --config config.yaml
 Future<void> main(List<String> args) async {
   final runner = CommandRunner('batch',
       'Runs analysis on multiple packages and compares granted score impact.')
     ..addCommand(BatchQueryCommand())
-    ..addCommand(BatchCompareCommand());
+    ..addCommand(BatchCompareCommand())
+    ..addCommand(BatchRunCommand());
   await runner.run(args);
 }
---
+++ lib/src/batch/batch_run_command.dart
@@ -0,0 +1,136 @@
+// Copyright (c) 2021, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+import 'package:http/http.dart' as http;
+import 'package:pana/src/package_analyzer.dart';
+import 'package:yaml/yaml.dart' as yaml;
+
+import '../download_utils.dart' show withTempDir;
+import '../sdk_env.dart' show ToolEnvironment;
+
+import 'batch_model.dart';
+
+/// Runs pana on the selected packages.
+class BatchRunCommand extends Command {
+  @override
+  String get name => 'run';
+
+  @override
+  String get description => 'Runs pana on the selected packages.';
+
+  BatchRunCommand() {
+    argParser
+      ..addOption(
+        'packages',
+        help:
+            'The file that contains the list of packages, or the comma separated names.',
+      )
+      ..addOption(
+        'config',
+        help: 'The configuration file to use.',
+      )
+      ..addOption(
+        'output',
+        help: 'Write the result to file (stdout otherwise).',
+      );
+  }
+
+  @override
+  Future<void> run() async {
+    final packages = await _parsePackages(argResults!['packages'] as String);
+    final config = await _parseConfig(argResults!['config'] as String);
+    final output = argResults!['output'] as String?;
+
+    await withTempDir((tempDir) async {
+      final env = await _initToolEnv(config, tempDir);
+      final options = await _parseOptions(config);
+
+      final results = <String, dynamic>{};
+
+      for (final package in packages) {
+        dynamic result;
+        try {
+          print('analyzing $package...');
+          final summary = await PackageAnalyzer(env)
+              .inspectPackage(package, options: options);
+          result = summary.report?.grantedPoints;
+        } catch (e, st) {
+          result = '$e\n$st';
+        }
+        if (result is! int) {
+          print('$package: $result');
+        }
+        results[package] = result;
+      }
+
+      final formatted = const JsonEncoder.withIndent('  ').convert(results);
+      if (output == null) {
+        print(formatted);
+      } else {
+        await File(output).writeAsString(formatted);
+      }
+    });
+  }
+
+  Future<List<String>> _parsePackages(String arg) async {
+    final file = File(arg);
+    if (file.existsSync()) {
+      return await file.readAsLines();
+    }
+    return arg.split(',').map((e) => e.trim()).toList();
+  }
+
+  Future<BatchConfig> _parseConfig(String? arg) async {
+    if (arg == null) {
+      return BatchConfig();
+    }
+    final file = File(arg);
+    if (file.existsSync()) {
+      var content = await file.readAsString();
+      if (file.path.endsWith('.yaml')) {
+        content = json.encode(yaml.loadYaml(content));
+      }
+      return BatchConfig.fromJson(json.decode(content) as Map<String, dynamic>);
+    }
+    throw ArgumentError('Unable to load config: `$arg`.');
+  }
+
+  Future<ToolEnvironment> _initToolEnv(
+      BatchConfig config, String pubCache) async {
+    return await ToolEnvironment.create(
+      dartSdkDir: config.dartSdk,
+      flutterSdkDir: config.flutterSdk,
+      environment: config.environment,
+      pubCacheDir: pubCache,
+    );
+  }
+
+  Future<InspectOptions> _parseOptions(BatchConfig config) async {
+    String? analysisOptionsYaml;
+    if (config.analysisOptions != null) {
+      if (config.analysisOptions!.startsWith('https://')) {
+        final rs = await http.get(Uri.parse(config.analysisOptions!));
+        if (rs.statusCode != 200) {
+          throw ArgumentError('Unable to access `${config.analysisOptions}`.');
+        }
+        analysisOptionsYaml = rs.body;
+      } else {
+        // local file
+        final file = File(config.analysisOptions!);
+        if (file.existsSync()) {
+          analysisOptionsYaml = await file.readAsString();
+        } else {
+          throw ArgumentError('Unable to access `${config.analysisOptions}`.');
+        }
+      }
+    }
+    return InspectOptions(
+      analysisOptionsYaml: analysisOptionsYaml,
+    );
+  }
+}
