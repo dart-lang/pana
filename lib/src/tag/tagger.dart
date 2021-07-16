5b97279847dbb10f045d19dcd74dd5b109dba3d0
István Soós <isoos@users.noreply.github.com>
2021-07-16 10:58:51 +0200

    Fix null-safety classification issue with dart-ext: imports. (#922)
    * Fix null-safety classification issue with dart-ext: imports.
    
    * Simpler implementation.
    
    * Report details on inaccessible import.

--- lib/src/tag/tagger.dart
+++ lib/src/tag/tagger.dart
@@ -380,6 +380,15 @@ class Tagger {
           final optOutViolationFinder = PathFinder<Uri>(
             LibraryGraph(_session, runtime.declaredVariables),
             (library) {
+              if (library.scheme == 'dart-ext') return null;
+              final resolvedPath = _session.uriConverter.uriToPath(library);
+              if (resolvedPath == null) {
+                return (path) => Explanation(
+                      'Unable to access import.',
+                      'Because:\n${LibraryGraph.formatPath(path)} where $library is inaccessible.',
+                      tag: _nullSafeTag,
+                    );
+              }
               final unit = parsedUnitFromUri(_session, library);
               if (unit == null) return null;
               final languageVersionToken = unit.languageVersionToken;
