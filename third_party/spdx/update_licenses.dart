64d41f30539fdceb50744720bbf380a897b531ba
Bharat Biradar <62872048+bharat-biradar@users.noreply.github.com>
2021-08-22 00:25:38 +0530

    normalize licenses having same text

--- third_party/spdx/update_licenses.dart
+++ third_party/spdx/update_licenses.dart
@@ -47,6 +47,8 @@ Future<void> writeFiles(Archive archive) async {
 }
 
 Future<void> removeUnnecessaryFiles() async {
+  var licenseMap = <String, String>{};
+
   final textDirectory =
       Directory('third_party/spdx/licenses/license-list-data-master/text');
   final spdxDirectory =
@@ -54,15 +56,91 @@ Future<void> removeUnnecessaryFiles() async {
 
   for (var entity in textDirectory.listSync()) {
     final file = File(entity.path);
-    final name = entity.uri.pathSegments.last;
+    final name = entity.uri.pathSegments.last.split('.txt').first;
+    final content = file.readAsStringSync();
 
     /// Ignore the deprecated licenses as the [SPDX guidelines][1]
     /// suggests that they should not be used.
     ///
     /// [1]: https://github.com/spdx/license-list-data/blob/master/accessingLicenses.md#deprecated-licenses
-    if (name.startsWith('deprecated_')) continue;
-    await file.copy('third_party/spdx/licenses/$name');
+    if (name.startsWith('deprecated_')) {
+      continue;
+    }
+
+    // Check for licenses having same text.
+    if (checkDuplicatePresent(name, licenseMap, content)) {
+      continue;
+    } else {
+      licenseMap[name] = content;
+    }
   }
 
   await spdxDirectory.delete(recursive: true);
+  final basePath = 'third_party/spdx/licenses/';
+
+  licenseMap.forEach((key, value) async {
+    final path = basePath + key + '.txt';
+    final file = File(path);
+    await file.create();
+    file.writeAsStringSync(value);
+  });
 }
+
+/// This routine tries to find exactly licenses having same text and stores them in a normalized license file.
+///
+/// For example as `AGPL-1.0-only` and `AGPL-1.0-or-later` have same text
+/// we store a single license called `AGPL-1.0` instead of two similar license
+/// text.
+bool checkDuplicatePresent(
+    String name, Map<String, String> map, String content) {
+  name = name.replaceAll(similarReg, '');
+
+  if (map.containsKey(name)) {
+    if (content == map[name]) {
+      return true;
+    }
+    return false;
+  }
+
+  if (map.containsKey(name + '-only')) {
+    checkIfSameContent(map, content, name + '-only');
+    return true;
+  }
+
+  if (map.containsKey(name + '-or-later')) {
+    checkIfSameContent(map, content, name + '-or-later');
+    return true;
+  }
+
+  if (map.containsKey(name + '-rfn')) {
+    checkIfSameContent(map, content, name + '-rfn');
+    return true;
+  }
+
+  if (map.containsKey(name + '-no-rfn')) {
+    checkIfSameContent(map, content, name + '-no-rfn');
+    return true;
+  }
+
+  if (map.containsKey(name + '-no-invariants-only')) {
+    checkIfSameContent(map, content, name + '-no-invariants-only');
+    return true;
+  }
+
+  if (map.containsKey(name + '-no-invariants-or-later')) {
+    checkIfSameContent(map, content, name + '-no-invariants-or-later');
+    return true;
+  }
+  return false;
+}
+
+void checkIfSameContent(Map<String, String> map, String content, String name) {
+  if (map[name] == content) {
+    map.remove(name);
+    map[name.replaceAll(similarReg, '')] = content;
+  }
+}
+
+final similarReg = RegExp(
+    r'(-only|-or-later|-rfn|-no-rfn|-no-invariants-only|-no-invariants-or-later)',
+    caseSensitive: false);
