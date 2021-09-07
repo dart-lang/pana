d803996a70682e84cc359903f24e22bed91ebd49
Istvan Soos <istvan.soos@gmail.com>
2021-09-07 15:09:48 +0200

    Move tests out of lib/

--- test/third_party/diff_match_patch/diff_match_patch_test.dart
+++ test/third_party/diff_match_patch/diff_match_patch_test.dart
@@ -4,6 +4,25 @@ import 'package:pana/src/third_party/diff_match_patch/diff.dart';
 import 'package:test/expect.dart';
 import 'package:test/test.dart';
 
+// The follwing test cases were forked from the google's diff-match-patch
+// dart library and hence the actual tests are in a separate "third_-party"
+// directory and we only call those tests from here.
+void main() {
+  testDiffCommonPrefix();
+  testDiffCommonSuffix();
+  testDiffCommonOverlap();
+  testDiffMain();
+  testDiffHalfMatch();
+  testDiffLineToChars();
+  testDiffCharsToLines();
+  testDiffCleanupMerge();
+  testDiffCleanupSemanticLosses();
+  testDiffCleanupSemantic();
+  testDiffBisect();
+  testWordDiffLevenshtein();
+  testCleanupEfficiency();
+}
+
 void testDiffMain() {
   var expected = <Diff>[];
   _testDiffMain(
