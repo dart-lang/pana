456743277333a23fd4068bd45058f804dbfa95a0
István Soós <isoos@users.noreply.github.com>
2022-01-28 10:09:26 +0100

    Fix build verify test and generated version. (#1009)
    * Fix build verify test and generated version.
    
    * increased timeout

--- test/ensure_build_test.dart
+++ test/ensure_build_test.dart
@@ -5,11 +5,15 @@ import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ensure_build', () {
-    // TODO(sigurdm): investigate why the build doesn't come clean on
-    // Windows.
-    if (!Platform.isWindows) {
-      expectBuildClean();
-    }
-  });
+  test(
+    'ensure_build',
+    () async {
+      // TODO(sigurdm): investigate why the build doesn't come clean on
+      // Windows.
+      if (!Platform.isWindows) {
+        await expectBuildClean();
+      }
+    },
+    timeout: const Timeout.factor(4),
+  );
 }
