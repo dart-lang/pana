2ba7ca9d897414738fb558774a024d77df5e5864
Istvan Soos <istvan.soos@gmail.com>
2022-08-17 10:26:59 +0200

    updated regexp

--- lib/src/license_detection/license.dart
+++ lib/src/license_detection/license.dart
@@ -6,7 +6,7 @@ part of 'license_detector.dart';
 
 /// Regular expression to match the common pattern of `Copyright (c) yyyy[-yyyy] <name>.`
 final _copyrightRegExp =
-    RegExp(r'^copyright.+\d{4}.{0,100}$', caseSensitive: false);
+    RegExp(r'^copyright\s+(?:(c)|©)\s+\d{4}.{0,100}$', caseSensitive: false);
 
 @sealed
 class License {
