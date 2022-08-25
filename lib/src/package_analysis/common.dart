7fe9d881a26fea2859c8d959a72337275c64ab94
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2022-08-25 16:06:00 +0100

    slim down lower_bound_constraint_analysis.dart

--- lib/src/package_analysis/common.dart
+++ lib/src/package_analysis/common.dart
@@ -308,3 +308,17 @@ Future<List<Version>> fetchSortedPackageVersionList({
   }
   return metadataStringToSortedVersions(metadataResponse.body);
 }
+
+/// Returns the package name from a library identifier/uri, or null if
+/// [libraryUri] isn't of the form `package:*/*`.
+///
+/// Example:
+/// `packageFromLibraryUri('package:my_package/my_library.dart')` returns
+/// `'my_package'`.
+String? packageFromLibraryUri(String libraryUri) {
+  if (libraryUri.startsWith('package:') && libraryUri.contains('/')) {
+    final endIndex = libraryUri.indexOf('/');
+    return libraryUri.substring(8, endIndex);
+  }
+  return null;
+}
