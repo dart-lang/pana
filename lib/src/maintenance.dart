ad4fc9655b1d51688699665147c28ea95d7afe86
Parker Lougheed <parlough@gmail.com>
2024-02-06 15:40:38 -0600

    Cleanup library directive usage

--- lib/src/maintenance.dart
+++ lib/src/maintenance.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library pana.maintenance;
-
 import 'package:json_annotation/json_annotation.dart';
 
 /// Returns the candidates in priority order to display under the 'Example' tab.
