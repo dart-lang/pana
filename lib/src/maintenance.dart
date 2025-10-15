c513ad7f36788a058c82c61c8ace85fd29738926
István Soós <isoos@users.noreply.github.com>
2025-10-15 12:05:34 +0200

    Cleanup: no need for special handling of CheckedFromJsonException (#1503)

--- lib/src/maintenance.dart
+++ lib/src/maintenance.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:json_annotation/json_annotation.dart';
-
 /// Returns the candidates in priority order to display under the 'Example' tab.
 List<String> exampleFileCandidates(String package) {
   return <String>[
@@ -22,17 +20,4 @@ List<String> exampleFileCandidates(String package) {
     'example/bin/example.dart',
     'example/example.dart',
   ];
-}
-
-/// Returns a markdown-formatted error message for pubspec.yaml parse error.
-String pubspecParseError(Object error) {
-  // TODO: remove this after json_annotation is updated with CheckedFromJsonException.toString()
-  var message = error.toString();
-  if (error is CheckedFromJsonException) {
-    final msg =
-        error.message ?? 'Error with `${error.key}`: ${error.innerError}';
-    message = 'CheckedFromJsonException: $msg';
-  }
-  return 'Error while parsing `pubspec.yaml`.\n\n'
-      'Parsing throws an exception:\n\n```\n$message\n```';
 }
