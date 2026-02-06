342a2c66fc6f24a156e731075d6bc1c87b6001d3
Istvan Soos <istvan.soos@gmail.com>
2026-02-06 15:28:06 +0100

    updated test

--- lib/src/sdk_env.dart
+++ lib/src/sdk_env.dart
@@ -7,7 +7,6 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:cli_util/cli_util.dart' as cli;
-import 'package:collection/collection.dart';
 import 'package:path/path.dart' as p;
 import 'package:pub_semver/pub_semver.dart';
 
@@ -17,6 +16,7 @@ import 'logging.dart';
 import 'model.dart' show PanaRuntimeInfo;
 import 'package_analyzer.dart' show InspectOptions;
 import 'sandbox_runner.dart';
+import 'tool/format_output_parser.dart';
 import 'tool/run_constrained.dart';
 import 'utils.dart';
 import 'version.dart';
@@ -312,68 +312,13 @@ class ToolEnvironment {
         return [];
       }
 
-      final dirPrefix = packageDir.endsWith('/') ? packageDir : '$packageDir/';
       final output = result.asJoinedOutput;
-      final lines = LineSplitter.split(result.asJoinedOutput).toList();
-      final changedFiles =
-          lines
-              .where((l) => l.startsWith('Changed'))
-              .map((l) => l.substring(8).replaceFirst(dirPrefix, '').trim())
-              .where(isAnalysisTarget)
-              .toList()
-            ..sort();
-
-      // clean exit of `dart format` with code = 1
-      // it should report at least one changed file
-      if (result.exitCode == 1 && changedFiles.isNotEmpty) {
-        return changedFiles;
-      }
-
-      // unclear exit, trying to retain only unexpected lines:
-      final unexpectedLines = lines
-          .where((l) => l.isNotEmpty)
-          // regular output
-          .whereNot((l) => l.startsWith('Changed') || l.startsWith('Formatted'))
-          // reference resolve warnings, can be ignored
-          .whereNot(
-            (l) =>
-                l.startsWith('Warning: Package resolution error') ||
-                l.startsWith('Failed to resolve package URI'),
-          )
-          // Issue with the format of the file, line can be ignored, details with be repeated later
-          .whereNot(
-            (l) => l.startsWith(
-              'Could not format because the source could not be parsed',
-            ),
-          )
-          // parse lines like `line 142, column 21 of /tmp/pana_CAMRUS/example/lib/main.dart: This requires the 'null-aware-elements' language feature to be enabled.`
-          .whereNot((l) {
-            if (!l.startsWith('line ')) return false;
-            final path = RegExp(
-              r'^line \d+, column \d+ of (/.+)\:',
-            ).matchAsPrefix(l)?.group(1)?.replaceFirst(dirPrefix, '');
-            if (path == null) return false;
-            if (isAnalysisTarget(path)) {
-              // keeping the line, since it may be important
-              return false;
-            } else {
-              // file is outside of the scope of analysis, e.g. `example/lib/main.dart`
-              return true;
-            }
-          })
-          // code example with line number prefix (e.g. ` <number> |`), can be ignored
-          .whereNot((l) => l.startsWith(RegExp(r'\s*\d*\s*[\|╷╵│]')))
-          .toList();
-
-      // all output lines are known and can be ignored
-      if (unexpectedLines.isEmpty) {
-        return changedFiles;
-      }
-
-      print(unexpectedLines);
-
-      final errorMsg = LineSplitter.split(output).take(20).join('\n');
-      throw ToolException(errorMsg, result);
+      return parseDartFormatOutput(
+        packageDir: packageDir,
+        exitCode: exitCode,
+        output: output,
+        result: result,
+      );
     });
   }
 
