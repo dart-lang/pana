cb81ceb950baa5c0046ee28e9e94358276ee24ff
Sigurd Meldgaard <sigurdm@google.com>
2026-08-05 13:34:54 +0200

    Prevent following symlinks when writing dartdoc_options.yaml and analysis_options.yaml (#1606)

--- test/sdk_env_test.dart
+++ test/sdk_env_test.dart
@@ -2,7 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:pana/src/sdk_env.dart';
+import 'package:pana/src/utils.dart';
+import 'package:path/path.dart' as p;
 import 'package:pub_semver/pub_semver.dart';
 import 'package:test/test.dart';
 
@@ -49,4 +53,40 @@ void main() {
       throwsA(isA<FormatException>()),
     );
   });
+
+  test(
+    'filesNeedingFormat handles dangling symlink analysis_options.yaml safely',
+    () async {
+      final toolEnv = await ToolEnvironment.create();
+      await withTempDir((dir) async {
+        final outsideDir = await Directory.systemTemp.createTemp('outside_');
+        try {
+          final outsideTarget = p.join(outsideDir.path, 'canary.txt');
+          final symlink = Link(p.join(dir, 'analysis_options.yaml'));
+          await symlink.create(outsideTarget);
+
+          final pubspec = File(p.join(dir, 'pubspec.yaml'));
+          await pubspec.writeAsString('''
+name: test_pkg
+version: 1.0.0
+environment:
+  sdk: '>=3.0.0 <4.0.0'
+''');
+          final libDir = Directory(p.join(dir, 'lib'))..createSync();
+          File(
+            p.join(libDir.path, 'test_pkg.dart'),
+          ).writeAsStringSync('void main() {}\n');
+
+          expect(await File(outsideTarget).exists(), isFalse);
+
+          await toolEnv.filesNeedingFormat(dir, false);
+
+          // Target file outside should not be created.
+          expect(await File(outsideTarget).exists(), isFalse);
+        } finally {
+          await outsideDir.delete(recursive: true);
+        }
+      });
+    },
+  );
 }
