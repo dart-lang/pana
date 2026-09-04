69e9039977e090ed251a859bbf2b7068ec01d118
Sigurd Meldgaard <sigurdm@google.com>
2026-09-04 13:32:52 +0000

    Update git branch detection tests after default branch migration

--- test/repository/git_local_repository_test.dart
+++ test/repository/git_local_repository_test.dart
@@ -26,14 +26,14 @@ void main() {
 
     test('master', () async {
       expect(
-        await getDefaultBranch('https://github.com/dart-lang/pana.git'),
+        await getDefaultBranch('https://github.com/dart-lang/build.git'),
         'master',
       );
     });
 
     test('main', () async {
       expect(
-        await getDefaultBranch('https://github.com/dart-lang/lints'),
+        await getDefaultBranch('https://github.com/dart-lang/pana.git'),
         'main',
       );
     });
